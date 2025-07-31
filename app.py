9ac683f0132e53245583706df0ebfc3949fd70f9
neehubee <neeharaplus@gmail.com>
2025-07-31 19:16:15 +0530

    cat

--- app.py
+++ app.py
@@ -47,4 +47,7 @@ def recommend():
     return jsonify(top_books.to_dict(orient='records'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port=int (os.environ.get('PORT',7860))
+    app.run(host='0.0.0.0',port=port,debug=False)
+
